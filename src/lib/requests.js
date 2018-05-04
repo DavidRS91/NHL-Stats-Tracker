c16bef9176cece8562ec04526a45ae696be511cb
David Scott <david.r.scott91@gmail.com>
2018-05-04 15:37:39 -0700

    add requests to fetch players from API

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,6 +1,6 @@
 const DOMAIN = "https://statsapi.web.nhl.com";
-const TEAMS_ENDPOINT = "api/v1/teams/";
-const PLAYERS_ENDPOINT = "api/v1/people/";
+const TEAMS_ENDPOINT = "api/v1/teams";
+const PLAYERS_ENDPOINT = "api/v1/people";
 const PLAYER_STATS_ENDPOINT = "?expand=person.stats&stats=yearByYear";
 
 async function getTeams() {
@@ -8,3 +8,16 @@ async function getTeams() {
   const teamsData = await teamsRequest.json();
   return teamsData.teams;
 }
+
+async function getAllPlayers() {
+  const teams = await getTeams();
+  let players = [];
+  for (const team of teams) {
+    const teamRequest = await fetch(`${DOMAIN}/${team.link}/roster`);
+    const teamData = await teamRequest.json();
+    players.push(...teamData.roster);
+  }
+  return players;
+}
+
+export default { getTeams, getAllPlayers };
